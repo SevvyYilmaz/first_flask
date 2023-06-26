d1a74eb4ff4dbaf4d6a83ebf37bf4de3ddce1a5a
SevvyYilmaz <yilmaz_S@outlook.de>
2023-06-25 21:48:51 -0700

    first commit

---
+++ friend.py
@@ -0,0 +1,57 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+class Friend:
+    #add this so you dont have to change more later
+    DB = "first_flask"
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('first_flask').query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append( cls(friend) )
+        return friends
+    
+
+    #save method will be used when we need to save a new friend to our DB 
+    @classmethod
+    def save(cls, data):
+        query = """INSERT INTO friends (first_name,last_name,occupation) VALUES (%(first_name)s,%(last_name)s,%(occ)s);"""
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        return result
+    
+    # the get_one method will be used when we need to retrieve just one specific row of the table
+    @classmethod
+    def get_one(cls, friend_id):
+        query  = "SELECT * FROM friends WHERE id = %(id)s";
+        data = {'id':friend_id}
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(results[0])
+    
+    # the update method will be used when we need to update a friend in our database
+    @classmethod
+    def update(cls,data):
+        query = """UPDATE friends 
+                SET first_name=%(first_name)s,
+                last_name=%(last_name)s,email=%(email)s 
+                WHERE id = %(id)s;"""
+        return connectToMySQL(cls.DB).query_db(query,data)
+    
+# the delete method will be used when we need to delete a friend from our database
+    @classmethod
+    def delete(cls, friend_id):
+        query  = "DELETE FROM friends WHERE id = %(id)s;"
+        data = {"id": friend_id}
+        return connectToMySQL(cls.DB).query_db(query, {"id" : id})
